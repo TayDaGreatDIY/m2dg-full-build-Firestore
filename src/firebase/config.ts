f552f47be9a457740fedccb7808ff3567c3fe593
Tay <married2dagame4444@gmail.com>
2025-10-26 22:19:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9256640302-2312c",
+  "appId": "1:305996615141:web:78a4befb75968e44e0730c",
+  "apiKey": "AIzaSyArbjBiN0NRKgDAKeqE-3IfxGeErQ7bWbQ",
+  "authDomain": "studio-9256640302-2312c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "305996615141"
+};
